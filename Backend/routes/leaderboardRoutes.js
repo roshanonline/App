25ecc7f2142a264050e97082ddffbc553a3290f2
roshanonline <roshanpost07at@email.com>
2025-04-19 23:46:35 +0530

    delete the files

--- Backend/routes/leaderboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getLeaderboard } = require('../controllers/leaderboardController');
-
-const router = express.Router();
-
-// Public route to get leaderboard data
-router.get('/', getLeaderboard); // e.g., GET /api/leaderboard?limit=20&filter=weekly
-
-module.exports = router;
